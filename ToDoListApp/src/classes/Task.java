91ee3a5100f568be8441d5a05c67bae1f3456749
youssef-awsys <youssef.cabanero@awsys-i.com>
2025-12-23 10:52:54 +0800

    Update markAsCompleted method to include logging

--- ToDoListApp/src/classes/Task.java
+++ ToDoListApp/src/classes/Task.java
@@ -44,7 +44,7 @@ public class Task {
 
     public void markAsCompleted() {
         this.isCompleted = true;
-        System.out.println("Task ID " + id + " marked as completed."); // Log completion
+        System.out.println("Task ID: " + id + " marked as completed."); // Log completion functionality
     }
 
     // Method to set the deadline with validation
